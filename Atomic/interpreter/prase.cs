06e8660d9294156d8d820e3e13b8b1b1fe023a46
ZeroBytes000 <116169309+ZeroBytes000@users.noreply.github.com>
2023-07-17 19:05:37 +0300

    parse 2 (contuine on phone)

--- Atomic/interpreter/prase.cs
+++ Atomic/interpreter/prase.cs
@@ -10,4 +10,26 @@ public class Parsing {
     public Parsing(List<(string value, TokenType type)> ions) {
         this.ions = ions;
     }
+    
+
+    //vars
+    public static line = 1;
+    public static column = 1;
+    public List<(string name, TokenType type, string value)> vars;
+    public void error(string message)
+	{
+		Console.BackgroundColor = ConsoleColor.Red;
+		Console.ForegroundColor = ConsoleColor.Yellow;
+
+		Console.WriteLine(message + "\nat => line:{0}, column:{1}", line, column);
+		Console.BackgroundColor = ConsoleColor.Black;
+		Console.ForegroundColor = ConsoleColor.White;
+        
+		Console.WriteLine("press anything to exit");
+		Console.ReadKey();
+		Environment.Exit(1);
+
+	}
+
+    
 }
