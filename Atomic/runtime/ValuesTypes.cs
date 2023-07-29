865f799d3e69259dfd9ee6b2af3d4630a8ca2a34
= <=>
2023-07-29 19:07:02 +0300

    RuntimeVal

--- Atomic/runtime/ValuesTypes.cs
+++ Atomic/runtime/ValuesTypes.cs
@@ -4,18 +4,22 @@ namespace ValueTypes;
 public class VT {
     public static string[] ValueType = new string[3] {"null", "number","string"};
 
-    public class NullVal {
-        static string type = ValueType[0];
-        static string value = "null";
+
+    public class RuntimeVal {
+        static string[] type = ValueType;
+    }
+    public class NullVal : RuntimeVal {
+        public static string type = ValueType[0];
+        public static string value = "null";
     }
 
 
-    public class NumValue : NullVal {
+    public class NumValue : RuntimeVal {
         public static string type = ValueType[1];
         public int value {get; set;}
     }
 
-    public class StringVal : NullVal {
+    public class StringVal : RuntimeVal {
         public static string type = ValueType[2];
 
         public string value {get; set;}
